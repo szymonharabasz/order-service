ac16ba74500babfa59ba15dff1bd11e7cd9c35bd
Szymon Harabasz <szymon.harabasz@gmail.com>
2023-05-04 02:33:40 +0200

    Implemented timeout, retry and fallbacks for the web client

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -1,9 +1,13 @@
 package com.polarbookshop.orderservice.book;
 
+import java.time.Duration;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 @Component
 public class BookClient {
@@ -19,6 +23,12 @@ public class BookClient {
             .get()
             .uri(BOOKS_ROOT_API + isbn)
             .retrieve()
-            .bodyToMono(Book.class);
+            .bodyToMono(Book.class)
+            .timeout(Duration.ofSeconds(3), Mono.empty())
+            .onErrorResume(WebClientResponseException.NotFound.class, 
+                exception -> Mono.empty())
+            .retryWhen(Retry.backoff(3, Duration.ofMillis(200)))
+            .onErrorResume(Exception.class, 
+                exception -> Mono.empty());
     }
 }
