eaf55ad7d5b30b919aecdf3e28f1b76323e9bb74
Szymon Harabasz <szymon.harabasz@gmail.com>
2023-05-10 12:39:59 +0200

    Added a delay before starting an application in order to wait for the Postgres container to start

--- src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java
+++ src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java
@@ -9,6 +9,12 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+		int secondsToSleep = 5;
+		try {	
+			Thread.sleep(secondsToSleep * 1000);
+		} catch (InterruptedException ie) {
+			Thread.currentThread().interrupt();
+		}
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
